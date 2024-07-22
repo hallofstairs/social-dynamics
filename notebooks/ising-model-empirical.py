874418b5ff63724e7317620c72f0d4ddccac2a6c
Jett Hollister <jett.hollister@duke.edu>
2024-07-22 19:00:29 -0400

    Init

--- notebooks/ising-model-empirical.py
+++ notebooks/ising-model-empirical.py
@@ -1,8 +1,67 @@
 # %% Imports
 
+import json
+import os
+import random
+import typing as t
+
 import raphtory as rp
 
+STREAM_DIR = "../data/stream"
+
 # %%
+
+
+g = rp.Graph()
+
+
+def init_belief():
+    return random.choice([-1, 1])
+
+
+def update_belief(n: rp.Node, p: rp.Properties):
+    p.as_dict()
+
+    return
+
+
+for day in sorted(os.listdir(STREAM_DIR)):
+    # Insert new information into graph
+    with open(f"{STREAM_DIR}/{day}", "r") as file:
+        for line in file:
+            record = json.loads(line.strip())
+
+            # Insert new nodes
+            if record["$type"] == "app.bsky.actor.profile":
+                g.add_node(
+                    timestamp=record["createdAt"],
+                    id=record["did"],
+                    properties={"belief": init_belief()},
+                )
+
+            # Draw new edges
+            elif record["$type"] == "app.bsky.graph.follow":
+                g.add_edge(
+                    timestamp=record["createdAt"],
+                    src=record["did"],
+                    dst=record["subject"],
+                )
+
+    # Update nodes
+    for node in t.cast(list[rp.Node], g.nodes):
+        # Get a random sample of
+        node.properties
+
+        # Chinatown soup: tea tasting
+
+    break
+
+
+# %%
+
+
+# %%
+
 
 # For every time t (daily):
 #   New data introduced:
